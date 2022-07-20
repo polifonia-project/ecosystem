248f515098983d837bf263fda7ff2314810d67c3
Enrico Daga <enrico.daga@open.ac.uk>
2022-07-20 12:12:05 +0100

    Develop #49

--- update-submodules.sh
+++ update-submodules.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
-for d in content/* ; do
-    cd "$d"
-    pwd
-    git pull
-    cd ../..
-done
+# for d in content/* ; do
+#     cd "$d"
+#     pwd
+#     git pull
+#     cd ../..
+# done
+
+# for d in content/* ; do
+#     cd "$d"
+#     pwd
+#     git pull
+#     cd ../..
+# done
+
+while IFS= read -r line; do
+    echo "Text read from file: $line"
+	IN="$line"
+	arrIN=(${IN//:/ })
+	repo=${arrIN[0]}
+	version=${arrIN[2]}
+	echo "repo: $repo"
+	echo "version: $version"     
+	[ -d "content/$repo" ] || git clone -n "https://github.com/$repo" "content/$repo"
+	cd "content/$repo" 
+	git ls-tree -r "$version" --full-tree --name-only | grep "\.md" > files.tmp
+	while IFS= read -r line; do 
+		git checkout "$version" "$line"
+	done < files.tmp
+	cd -
+done < repositories.txt
